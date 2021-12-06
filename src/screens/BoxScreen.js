90229bc726ce2c1536ade385d7434ce8f7c0466b
Chris Morris <cmorris@Chriss-iMac-2.local>
2021-12-06 11:39:08 +0000

    Final commit

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box Screen</Text>
+            <View style={styles.ViewOneStyle} />
+            <View style={styles.ViewTwoStyle} />
+            <View style={styles.ViewThreeStyle} />
         </View>
     )
 };
@@ -12,12 +14,30 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
-        borderColor: 'black'
-    },
-    textStyle: {
-        borderWidth: 1,
         borderColor: 'red',
-        margin: 40
+        height: 400,
+        justifyContent: 'space-between'
+    },
+    viewOneStyle: {
+        height: 50,
+        width: 50,
+        borderWidth: 2,
+        borderColor: 'red',
+        backgroundColor: 'red'
+    },
+    viewTwoStyle: {
+        height: 50,
+        width: 50,
+        borderWidth: 1,
+        borderColor: 'blue',
+        backgroundColor: 'blue'
+    },
+    viewThreeStyle: {
+        height: 50,
+        width: 50,
+        borderWidth: 1,
+        borderColor: 'green',
+        backgroundColor: 'green'
     }
 });
 
